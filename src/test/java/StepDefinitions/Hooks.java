261e7dd3717c143222ac49ef297bc89cb221e9aa
mazlumoz <gsmazlum04@gmail.com>
2024-10-27 21:29:46 +0300

    Duzeltme.

--- src/test/java/StepDefinitions/Hooks.java
+++ src/test/java/StepDefinitions/Hooks.java
@@ -9,17 +9,16 @@ import org.openqa.selenium.TakesScreenshot;
 
 public class Hooks {
 
-    // Cucumbera ait her senaryodan sonra çalışan
-    // Annotation
+
     @After
-    public void after(Scenario senaryo) // Cucumberın ototmarik senaryo ile ilgili bilgiler değişkeni
+    public void after(Scenario senaryo)
     {
         ExcelUtility.writeToExcel("src/test/java/ApachePOI/resource/CucumberTestSonuçlari.xlsx",
                 senaryo.getName(),
                 senaryo.isFailed() ? "Fail": "Passed"
         );
 
-        // aşağıdaki bölüm sadece extend report plugini devrede ise açılır
+
         if (senaryo.isFailed()){
             TakesScreenshot ts=((TakesScreenshot) GWD.getDriver());
             byte[] hafizadakiHali=ts.getScreenshotAs(OutputType.BYTES);
